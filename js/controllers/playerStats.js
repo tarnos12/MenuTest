728b661f48992139f7489b69482ad20439ae2672
Mariusz Klimek <tarnos7@gmail.com>
2016-01-14 21:46:33 +0100

    test

---
+++ js/controllers/playerStats.js
@@ -0,0 +1,30 @@
+﻿"use strict";
+testApp.controller('PlayerStats', ['$scope', '$http', function ($scope, $http) {
+    $http.get('stats/stats.json').success(function (data) {
+        $scope.stats = data;
+    });
+    $scope.orderByStrength = 'Strength';
+    $scope.gameMenu = {
+        loadingScreen:{
+            state:true,
+        },
+        startMen: {
+            state: true,
+        },
+        charSelect: {
+            state: false,
+            toggle: function () {
+                this.state = !this.state;
+                $scope.gameMenu.startMen.state = !this.state;
+            }
+        },
+        charCreate: {
+            state: false,
+            toggle: function () {
+                this.state = !this.state;
+                $scope.gameMenu.charSelect.state = !this.state;
+            }
+        }
+    };
+
+}]);
---
+++ js/main.js
@@ -0,0 +1,1 @@
+﻿var testApp = angular.module('testApp', []);
